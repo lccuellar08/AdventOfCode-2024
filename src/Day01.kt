5b43e2dffeff6811b8f7681137371447ef2ef596
Luis Cuellar <lccuellar08@gmail.com>
2024-12-01 10:35:06 -0700

    Initial commit test

--- src/Day01.kt
+++ src/Day01.kt
@@ -12,10 +12,4 @@ fun main() {
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
 }
